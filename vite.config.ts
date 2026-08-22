6a4cceef9ae082b4eb87ad6c43472329adcf280c
mayonelmark-jpg <mayonelmark@gmail.com>
2026-08-23 00:43:52 +0800

    Update vite.config.ts
    Commit change

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs.plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Activity1_Mayo_Repository/',
   plugins: [
     react(),
-      tailwindcss(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
